880f72a55b572415b7f3fcd8a888fc9942898692
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2023-09-29 09:19:32 +0530

    Initial commit

---
+++ aws-cloud-rds/src/main/java/com/bytewink/awscloudrds/AwsCloudRdsApplication.java
@@ -0,0 +1,11 @@
+package com.bytewink.awscloudrds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCloudRdsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCloudRdsApplication.class, args);
+    }
+}
